36439060b56047b5d1ac29e79d264a83520f6651
dshin121 <dshin121@vt.edu>
2022-11-29 13:48:34 -0500

    Update rhythm.js

--- rhythm.js
+++ rhythm.js
@@ -2,6 +2,7 @@
 //with the Music class which includes our tilemap and button checking for score implementation that is mainly our game class state.
 //Jihoon contributed with the tilemap, button press visualization for the game class state, and incorporating the game play state from the main menu state.
 
+
 //This class creates the note object in order to play the game
 //in the play state.
 class Note {
@@ -23,101 +24,70 @@ class Note {
   //note objects in order to play the game, and button checking
   //implmentation in order to score points
   class Music {
-	constructor(speed) {
+	constructor(speed, text) {
 	  this.speed = speed;
 	  this.score = 0;
-	  this.streak = 0;
 	  //reading from txt file
-	  this.tilemap = txt;
+	  this.tilemap = text;
+	  this.notes = [];
 	}
 	initialize() {
 	  for (var i = 0; i < this.tilemap.length; i++) {
 		for (var j = 0; j < this.tilemap[i].length; j++) {
 		  switch (this.tilemap[i][j]) {
 			case "1":
-			  notes.push(
-				new Note(j * 60 + 50, i * 30 - this.tilemap.length * 30)
-			  );
+			  this.notes.push(new Note(j * 60 + 50, i * 30 - this.tilemap.length * 30));
+			  break;
+			case "2":
+			  this.notes.push(new Note(j * 60 + 50, i * 30 - this.tilemap.length * 30 - 10));
+			  break;
+			case "3":
+			  this.notes.push(new Note(j * 60 + 50, i * 30 - this.tilemap.length * 30 - 20));
 			  break;
 		  }
 		}
 	  }
 	}
 	move() {
-	  for (var i = 0; i < notes.length; i++) {
-		notes[i].y += this.speed;
+	  for (var i = 0; i < this.notes.length; i++) {
+		this.notes[i].y += this.speed;
 	  }
 	}
 	checkPlay() {
-	  this.scorestreak()
-	  for (var i = 0; i < notes.length; i++) {
-		if (notes[i].y >= 335 && notes[i].y <= 365 && notes[i].played === 0) {
-		  if (keyArray[65] === 1 && notes[i].x === 50) {
-			notes[i].played = 1;
+	  for (var i = 0; i < this.notes.length; i++) {
+		if (this.notes[i].y >= 335 && this.notes[i].y <= 365 && this.notes[i].played === 0) {
+		  if (keyArray[65] === 1 && this.notes[i].x === 50) {
+			this.notes[i].played = 1;
 			this.score++;
-			this.streak++;
 		  }
-		  if (keyArray[83] === 1 && notes[i].x === 110) {
-			notes[i].played = 1;
+		  if (keyArray[83] === 1 && this.notes[i].x === 110) {
+			this.notes[i].played = 1;
 			this.score++;
-			this.streak++;
 		  }
-		  if (keyArray[68] === 1 && notes[i].x === 170) {
-			notes[i].played = 1;
+		  if (keyArray[68] === 1 && this.notes[i].x === 170) {
+			this.notes[i].played = 1;
 			this.score++;
-			this.streak++;
 		  }
-		  if (keyArray[74] === 1 && notes[i].x === 230) {
-			notes[i].played = 1;
+		  if (keyArray[74] === 1 && this.notes[i].x === 230) {
+			this.notes[i].played = 1;
 			this.score++;
-			this.streak++;
 		  }
-		  if (keyArray[75] === 1 && notes[i].x === 290) {
-			notes[i].played = 1;
+		  if (keyArray[75] === 1 && this.notes[i].x === 290) {
+			this.notes[i].played = 1;
 			this.score++;
-			this.streak++;
 		  }
-		  if (keyArray[76] === 1 && notes[i].x === 350) {
-			notes[i].played = 1;
+		  if (keyArray[76] === 1 && this.notes[i].x === 350) {
+			this.notes[i].played = 1;
 			this.score++;
-			this.streak++;
 		  }
 		}
-		if (notes[i].y >= 400 && notes[i].played === 0) {
+		if (this.notes[i].y >= 400 && this.notes[i].played === 0) {
 		  this.score--;
-		  this.streak = 0; //reset the streak
-		  notes[i].played = 1;
+		  this.notes[i].played = 1;
 		}
 	  }
 	}
-	scorestreak() {
-	  if (this.streak >= 10 && this.streak <= 29) {  //20 39
-		fill(255)
-		ellipse(random() * width, random() * height, 3, 3);
-		ellipse(random() * width, random() * height, 5, 5);
-	  } 
-	  else if (this.streak >= 30 && this.streak <= 49) {  //40 59
-		//fill(200, 164.7, 0); //orange
-		fill(240, 225, 48); //Dandelion Yellow
-		ellipse(random() * width, random() * height, 2, 2);
-		ellipse(random() * width, random() * height, 3, 3);
-		ellipse(random() * width, random() * height, 5, 5);
-	  }
-	  else if (this. streak >= 50){ //60
-		fill(135, 206, 235); //Sky Blue
-		ellipse(random() * width, random() * height, 1, 1);
-		ellipse(random() * width, random() * height, 2, 2);
-		ellipse(random() * width, random() * height, 3, 3);
-		ellipse(random() * width, random() * height, 4, 4);
-		ellipse(random() * width, random() * height, 5, 5);
-		ellipse(random() * width, random() * height, 1, 1);
-		ellipse(random() * width, random() * height, 2, 2);
-		ellipse(random() * width, random() * height, 3, 3);
-		ellipse(random() * width, random() * height, 4, 4);
-		ellipse(random() * width, random() * height, 5, 5);
-	  }
-	}
-  } //End music class
+  }
   
   //music note for the animation in the instruction screen
   class instructionNote {
@@ -127,12 +97,11 @@ class Note {
 	  this.id = id;
 	}
 	draw() {
-	  if (this.id == 0) {
-		//eighth note
+	  if (this.id == 0) { //eighth note
 		eighth.resize(40, 40);
 		image(eighth, this.x - 10, this.y - 10);
-	  } else {
-		//quarter note
+	  }
+	  else { //quarter note
 		quarter.resize(40, 40);
 		image(quarter, this.x - 10, this.y - 10);
 	  }
@@ -142,7 +111,7 @@ class Note {
 	}
   }
   
-  //the main menu screen state that contains the functionallity
+  //the main menu screen state that contains the functionallity 
   //for the keys
   class MenuScreenState {
 	constructor() {
@@ -156,78 +125,78 @@ class Note {
 	  this.l = 0;
 	}
 	execute(me) {
-	  if (keyArray[65] === 1 && this.a == 0) {
-		//a
+	  if (keyArray[65] === 1 && this.a == 0) { //a
 		fill(150);
 		circle(this.x - 170, this.y, 30);
 		addCircle(this.x - 170, this.y);
 		this.a = 1;
-	  } else {
+	  }
+	  else {
 		if (keyArray[65] === 0) {
 		  this.a = 0;
 		}
 		fill(255);
 		circle(this.x - 170, this.y, 30);
 	  }
-	  if (keyArray[83] === 1 && this.s == 0) {
-		//s
+	  if (keyArray[83] === 1 && this.s == 0) { //s
 		fill(150);
 		circle(this.x - 100, this.y, 30);
 		addCircle(this.x - 100, this.y);
 		this.s = 1;
-	  } else {
+	  }
+	  else {
 		if (keyArray[83] === 0) {
 		  this.s = 0;
 		}
 		fill(255);
 		circle(this.x - 100, this.y, 30);
 	  }
-	  if (keyArray[68] === 1 && this.d == 0) {
-		//d
+	  if (keyArray[68] === 1 && this.d == 0) { //d
 		fill(150);
 		circle(this.x - 30, this.y, 30);
 		addCircle(this.x - 30, this.y);
 		this.d = 1;
-	  } else {
+	  }
+	  else {
 		if (keyArray[68] === 0) {
 		  this.d = 0;
 		}
 		fill(255);
-		circle(this.x - 30, this.y, 30);
+		circle(this.x - 30, this.y, 30)
 	  }
-	  if (keyArray[74] === 1 && this.j == 0) {
-		//j
+	  if (keyArray[74] === 1 && this.j == 0) { //j
 		fill(150);
 		circle(this.x + 30, this.y, 30);
 		addCircle(this.x + 30, this.y);
 		this.j = 1;
-	  } else {
+	  }
+	  else {
 		if (keyArray[74] === 0) {
 		  this.j = 0;
 		}
 		fill(255);
 		circle(this.x + 30, this.y, 30);
 	  }
-	  if (keyArray[75] === 1 && this.k == 0) {
-		//k
+	  if (keyArray[75] === 1 && this.k == 0) { //k
 		fill(150);
 		circle(this.x + 100, this.y, 30);
 		addCircle(this.x + 100, this.y);
 		this.k = 1;
-	  } else {
+	  }
+	  else {
 		if (keyArray[75] === 0) {
 		  this.k = 0;
 		}
 		fill(255);
 		circle(this.x + 100, this.y, 30);
 	  }
-	  if (keyArray[76] === 1 && this.l == 0) {
-		//l
+	  if (keyArray[76] === 1 && this.l == 0) { //l
 		fill(150);
 		circle(this.x + 170, this.y, 30);
 		addCircle(this.x + 170, this.y);
 		this.l = 1;
-	  } else {
+	  }
+	  else {
 		if (keyArray[76] === 0) {
 		  this.l = 0;
 		}
@@ -236,7 +205,7 @@ class Note {
 	  }
 	  fill(0);
 	  text("A", this.x - 176, this.y + 7);
-	  text("S", this.x - 106, this.y + 7);
+	  text("S", this.x - 106, this.y + 7)
 	  text("D", this.x - 36, this.y + 7);
 	  text("J", this.x + 24, this.y + 7);
 	  text("K", this.x + 94, this.y + 7);
@@ -245,8 +214,7 @@ class Note {
   }
   
   // Circle class that expands
-  class expandCircleObj {
-	//ripple that expands when key is pressed
+  class expandCircleObj { //ripple that expands when key is pressed
 	constructor(x, y, r, g, b) {
 	  this.x = x;
 	  this.y = y;
@@ -266,8 +234,7 @@ class Note {
 	  this.red -= 5;
 	  this.blue -= 5;
 	  this.green -= 5;
-	  if (this.red <= 10) {
-		//when color reaches below 10 stop drawing
+	  if (this.red <= 10) { //when color reaches below 10 stop drawing
 		this.draw = 0;
 	  }
 	  pop();
@@ -278,6 +245,7 @@ class Note {
 	}
   }
   
+  
   class targetObj {
 	constructor(x, y) {
 	  this.x = x;
@@ -285,7 +253,7 @@ class Note {
 	}
   }
   
-  var mode; //song selection screen state
+  var mode;  //difficulty selection screen state?
   var start;
   var instruction;
   var target;
@@ -300,11 +268,13 @@ class Note {
   var quarter;
   var currFrameCount = 0;
   var count = 0;
-  var notes = [];
   var music;
   let song;
   var startost = 0;
   var txt = 0;
+  var sel;
+  var music2;
+  let song2;
   
   var mouseClicked = function () {
 	target.x = mouseX;
@@ -318,9 +288,8 @@ class Note {
 	keyArray[keyCode] = 0;
   }
   
-  function addCircle(x, y) {
-	//adds a circle to the key
-	c[index] = new expandCircleObj(x, y, 255, 255, 255);
+  function addCircle(x, y) { //adds a circle to the key
+	c[index] = (new expandCircleObj(x, y, 255, 255, 255));
 	index++;
 	if (index > 60) {
 	  index = 0;
@@ -328,8 +297,10 @@ class Note {
   }
   
   function preload() {
-	song = loadSound("poke.mp3");
+	song = loadSound('poke.mp3');
+	song2 = loadSound('sheep.mp3');
 	txt = loadStrings("tilemap.txt");
+	txt2 = loadStrings("sheep.txt");
   }
   
   function setup() {
@@ -340,9 +311,12 @@ class Note {
 	start = 0;
 	instruction = 0;
 	playgame = 0;
-	music = new Music(6.868);
+	music = new Music(6.868, txt);
 	music.initialize();
 	song.setVolume(0.5);
+	music2 = new Music(11.7, txt2);
+	music2.initialize();
+	song2.setVolume(0.3);
 	//draws the eighth and quarter note to save
 	noStroke();
 	rect(129, 130, 10, 170);
@@ -372,18 +346,17 @@ class Note {
   function draw() {
 	background(0);
 	if (start === 0) {
-	  for (var i = 0; i < c.length; i++) {
-		//ripples
+	  for (var i = 0; i < c.length; i++) { //ripples
 		if (c[i].draw == 1) {
 		  c[i].display();
 		  c[i].update();
 		}
 	  }
 	  gamestate[0].execute(gamestate[0]);
-	  fill(255);
+	  fill(255)
 	  textSize(30);
 	  //text("Thursday Night Thrillin'", 50, 100)
-	  text("JavaS. Beats", 120, 100);
+	  text("JavaS. Beats", 120, 100)
 	  //text("Ripples", 150, 100)
 	  textSize(15);
 	  text("Created by:", 160, 350);
@@ -394,22 +367,11 @@ class Note {
 	  //   text("y clicked" + target.y, 20, 30);
 	  // }
 	  //mouseClicked();
-	  if (
-		target.x >= 138 &&
-		target.x <= 233 &&
-		target.y >= 291 &&
-		target.y <= 299 &&
-		start === 0
-	  ) {
+	  if (target.x >= 138 && target.x <= 233 && target.y >= 291 && target.y <= 299 && start === 0) {
 		//print("Instructions clicked");
 		start = 1;
-	  } else if (
-		target.x >= 165 &&
-		target.y >= 256 &&
-		target.x <= 200 &&
-		target.y <= 270 &&
-		start === 0
-	  ) {
+	  }
+	  else if (target.x >= 165 && target.y >= 256 && target.x <= 200 && target.y <= 270 && start === 0) {
 		//start = 2;
 		//startost = frameCount;
 		start = 5; //5 is unused condition in code
@@ -417,24 +379,24 @@ class Note {
 	  }
 	  text("Play", 180, 270);
 	  text("Instructions", 145, 300);
-	} else if (start === 1) {
+	}
+	else if (start === 1) {
 	  //mouseClicked();.
 	  fill(255);
 	  rect(144, 50, 110, 100);
-	  if (currFrameCount < frameCount - 60) {
-		//indicates which notes appear
+	  if (currFrameCount < frameCount - 60) { //indicates which notes appear
 		switch (count) {
 		  case 0:
 			images[count] = new instructionNote(105, 114, 0);
 			break;
 		  case 1:
-			images[count] = new instructionNote(275, 114, 1);
+			images[count] = (new instructionNote(275, 114, 1));
 			break;
 		  case 2:
-			images[count] = new instructionNote(115, 114, 1);
+			images[count] = (new instructionNote(115, 114, 1));
 			break;
 		  case 3:
-			images[count] = new instructionNote(265, 114, 0);
+			images[count] = (new instructionNote(265, 114, 0));
 			break;
 		}
 		count++;
@@ -443,19 +405,18 @@ class Note {
 		}
 		currFrameCount = frameCount;
 	  }
-	  for (var i = 0; i < images.length; i++) {
-		//draws notes
+	  for (var i = 0; i < images.length; i++) { //draws notes
 		images[i].draw();
 		images[i].move();
 	  }
 	  strokeWeight(0);
 	  fill(137, 207, 240);
 	  ellipse(200, 160, 270, 14);
-	  fill(240, 225, 48);
-	  ellipse(200, 160, 270, 8);
+	  fill(240, 225, 48)
+	  ellipse(200, 160, 270, 8)
 	  fill(128, 0, 0);
 	  ellipse(200, 160, 270, 4);
-	  fill(255);
+	  fill(255)
 	  //spkr1
 	  stroke(0);
 	  strokeWeight(1);
@@ -467,7 +428,7 @@ class Note {
 	  circle(275, 114, 20);
 	  circle(275, 138, 20);
   
-	  fill(255);
+	  fill(255)
 	  square(200, 110, 20);
 	  square(210, 130, 20);
 	  square(190, 130, 20);
@@ -488,6 +449,7 @@ class Note {
   
 	  textSize(15);
 	  fill(255);
+	  //text("THIS GAME IS SCUFFED AF", 0, 200);
 	  text("Ripples is a rhythm game that tests the player's", 5, 215);
 	  text("ability to match button presses at a certain time.", 5, 230);
 	  text("Failure to time button presses will slowly bring", 5, 245);
@@ -501,12 +463,7 @@ class Note {
 	  text("Controls: Use ASDJKL keys to hit the note at the correct", 5, 350);
 	  text("timing in order to score points.", 5, 365);
 	  text("Return", 340, 380);
-	  if (
-		target.x >= 340 &&
-		target.y >= 370 &&
-		target.x <= 382 &&
-		target.y <= 380
-	  ) {
+	  if (target.x >= 340 && target.y >= 370 && target.x <= 382 && target.y <= 380) {
 		start = 0;
 	  }
 	  // if (target.x >= 0 && target.y >= 0){
@@ -515,91 +472,108 @@ class Note {
 	  //   start = 1;
 	  //   instruction = 0;
 	  // }
-	} else if (start === 2) {
+	}
+	else if (start === 2) {
 	  //text("I AM GAMING", 0, 200);
 	  images = [];
 	  c = [];
-	  if (frameCount === startost + 19) {
-		song.play();
+	  if (sel === 0) {
+		if (frameCount === startost + 19) {
+		  song.play();
+		}
+		background(0);
+		music.move();
+		music.checkPlay();
+		for (var i = 0; i < music.notes.length; i++) {
+		  if (music.notes[i].played === 0 && music.notes[i].y >= -30) {
+			music.notes[i].draw();
+		  }
+		}
 	  }
-	  background(0);
-	  music.move();
-	  music.checkPlay();
-	  for (var i = 0; i < notes.length; i++) {
-		if (notes[i].played === 0 && notes[i].y >= -30) {
-		  notes[i].draw();
+	  else if (sel === 1) {
+		//print(frameCount + " " + startost);
+		if (frameCount === startost + 70) {
+		  song2.play();
+		}
+		background(0);
+		music2.move();
+		music2.checkPlay();
+		for (var i = 0; i < music2.notes.length; i++) {
+		  if (music2.notes[i].played === 0 && music2.notes[i].y >= -30) {
+			music2.notes[i].draw();
+		  }
 		}
 	  }
 	  stroke(255);
 	  line(0, 350, 400, 350);
   
-	  if (keyArray[65] === 1) {
-		//A
+	  if (keyArray[65] === 1) { //A
 		fill(178, 190, 181);
 		circle(50, 350, 30);
 		fill(255);
 		text("A", 44, 355);
-	  } else {
+	  }
+	  else {
 		fill(255);
 		circle(50, 350, 30);
 		fill(0);
 		text("A", 44, 355);
 	  }
-	  if (keyArray[83] === 1) {
-		//S
+	  if (keyArray[83] === 1) { //S
 		fill(178, 190, 181);
 		circle(110, 350, 30);
 		fill(255);
 		text("S", 104, 355);
-	  } else {
+	  }
+	  else {
 		fill(255);
 		circle(110, 350, 30);
 		fill(0);
 		text("S", 104, 355);
 	  }
-	  if (keyArray[68] === 1) {
-		//D
+	  if (keyArray[68] === 1) { //D
 		fill(178, 190, 181);
 		circle(170, 350, 30);
 		fill(255);
 		text("D", 164, 355);
-	  } else {
+	  }
+	  else {
 		fill(255);
 		circle(170, 350, 30);
 		fill(0);
 		text("D", 164, 355);
 	  }
-	  if (keyArray[74] === 1) {
-		//J
+	  if (keyArray[74] === 1) { //J
 		fill(178, 190, 181);
 		circle(230, 350, 30);
 		fill(255);
 		text("J", 224, 355);
-	  } else {
+	  }
+	  else {
 		fill(255);
 		circle(230, 350, 30);
 		fill(0);
 		text("J", 224, 355);
 	  }
-	  if (keyArray[75] === 1) {
-		//K
+	  if (keyArray[75] === 1) { //K
 		fill(178, 190, 181);
 		circle(290, 350, 30);
 		fill(255);
 		text("K", 284, 355);
-	  } else {
+	  }
+	  else {
 		fill(255);
 		circle(290, 350, 30);
 		fill(0);
 		text("K", 284, 355);
 	  }
-	  if (keyArray[76] === 1) {
-		//L
+	  if (keyArray[76] === 1) { //L
 		fill(178, 190, 181);
 		circle(350, 350, 30);
 		fill(255);
 		text("L", 344, 355);
-	  } else {
+	  }
+	  else {
 		fill(255);
 		circle(350, 350, 30);
 		fill(0);
@@ -618,44 +592,31 @@ class Note {
 	  fill(255);
 	  textSize(15);
 	  text("Score: " + music.score, 10, 15);
-	  text("x" + music.streak, 360, 15);
-	} else if (mode === 1) {
+	}
+	else if (mode === 1) {
 	  textSize(20);
 	  fill(255);
 	  text("Song Selection", 130, 30);
 	  text("Mewmore // National Park", 80, 170);
-	  if (
-		target.x >= 80 &&
-		target.y >= 160 &&
-		target.x <= 315 &&
-		target.y <= 170
-	  ) {
+	  if (target.x >= 80 && target.y >= 160 && target.x <= 315 && target.y <= 170) {
 		start = 2;
+		sel = 0;
 		startost = frameCount;
 		mode = 0;
 	  }
 	  text("Chroma - Dark Sheep", 95, 210);
-	  if (
-		target.x >= 95 &&
-		target.y >= 200 &&
-		target.x <= 290 &&
-		target.y <= 210
-	  ) {
-		start = 0;
+	  if (target.x >= 95 && target.y >= 200 && target.x <= 290 && target.y <= 210) {
+		start = 2;
+		sel = 1;
+		startost = frameCount;
 		mode = 0;
 	  }
   
 	  textSize(15);
 	  text("Return", 340, 380);
-	  if (
-		target.x >= 340 &&
-		target.y >= 370 &&
-		target.x <= 382 &&
-		target.y <= 380
-	  ) {
+	  if (target.x >= 340 && target.y >= 370 && target.x <= 382 && target.y <= 380) {
 		start = 0; //return to title screen
 		mode = 0;
 	  }
 	}
   }
-  
